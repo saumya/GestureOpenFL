bdc239d3ac51afb9934e1443820ff0c938aa2a17
saumya <2saumyaray@gmail.com>
2017-02-26 16:10:51 +0530

    wip, new mobile component

--- Source/com/saumya/app/AppShell.hx
+++ Source/com/saumya/app/AppShell.hx
@@ -6,6 +6,7 @@ import openfl.display.DisplayObject;
 import openfl.display.Sprite;
 
 import com.saumya.component.MobilePanel;
+import com.saumya.component.AppScreen;
 
 
 class AppShell {
@@ -20,10 +21,10 @@ class AppShell {
 
 	public function init(view:Sprite):DisplayObject{
 		trace('TODO: Render the Application');
-		var s1:MobilePanel = new MobilePanel(this.screenWidth,this.screenHeight,0xFF0000);
-		var s2:MobilePanel = new MobilePanel(this.screenWidth,this.screenHeight,0x00FF00);
-		var s3:MobilePanel = new MobilePanel(this.screenWidth,this.screenHeight,0x0000FF);
-		var s4:MobilePanel = new MobilePanel(this.screenWidth,this.screenHeight,0xFF00FF);
+		var s1:AppScreen = new AppScreen(this.screenWidth,this.screenHeight,0xFF0000);
+		var s2:AppScreen = new AppScreen(this.screenWidth,this.screenHeight,0x00FF00);
+		var s3:AppScreen = new AppScreen(this.screenWidth,this.screenHeight,0x0000FF);
+		var s4:AppScreen = new AppScreen(this.screenWidth,this.screenHeight,0xFF00FF);
 		view.addChild(s1);
 		view.addChild(s2);
 		view.addChild(s3);
---
+++ Source/com/saumya/component/AppScreen.hx
@@ -0,0 +1,19 @@
+package com.saumya.component;
+
+import com.saumya.component.MobilePanel;
+
+class AppScreen extends MobilePanel {
+	
+	public function new(screenWidth:Float,screenHeight:Float,bgColor:UInt=0x440000) {
+		super(screenWidth,screenHeight,bgColor);
+	}
+	
+	override public function init():Void{
+		super.init();
+	}
+
+	override public function render():Void{
+		super.render();
+	}
+
+}
